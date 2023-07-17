46e5f88ee8f6e3489204bac77e66e9ccfa276a61
hhodek <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>
2023-07-17 16:10:36 -0500

    Flask setup

--- SurfsUp/app.py
+++ SurfsUp/app.py
@@ -1,15 +1,15 @@
+#################################################
+# Import Dependencies
+#################################################
 from flask import Flask, jsonify
 from sqlalchemy import create_engine, func
 from sqlalchemy.ext.automap import automap_base
 from sqlalchemy.orm import Session
 
-# Create an instance of Flask
-app = Flask(__name__)
 
 #################################################
 # Database Setup
 #################################################
-
 # Create engine to connect to SQLite database
 engine = create_engine("sqlite://hawaii.sqlite")
 
@@ -23,11 +23,13 @@ Station = Base.classes.station
 
 # Create a session (link) from Python to the database
 session = Session(engine)
+
+
 #################################################
 # Flask Setup
 #################################################
-
-
+# Create an instance of Flask
+app = Flask(__name__)
 
 
 #################################################
